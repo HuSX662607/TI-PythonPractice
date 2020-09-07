fcfbb3f84592029301bb0bf53485e94ce676ec47
SXH662607 <48062767+HuSX662607@users.noreply.github.com>
2020-09-07 21:17:19 +0800

    Add files via upload
    The Python script where the Pauli matrices are defined. If other programs are to use Pauli matrices, just import this script.

---
+++ PauliMatrices.py
@@ -0,0 +1,12 @@
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+
+I=0+1j #定义虚数单位
+sx=np.array([[0,1],
+             [1,0]])
+sy=np.array([[0,-I],
+             [I,0]])
+sz=np.array([[1,0],
+             [0,-1]])
+
